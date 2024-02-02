5e1a552083327b9b7e5987987f91d4d7fd07f2f3
FernandoEmilianoFloresDeLaRiva <emilianoflores07081@gmail.com>
2024-02-02 09:22:56 -0600

    -

--- app.ts
+++ app.ts
@@ -2,14 +2,49 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 
 const app = express();
-const PORT = "3000";
 
-app.get("/", (req : Request, res : Response) => {
-  try{
-    res.status(200).json("hola :)")
-  }catch(err : any){
-    res.status(500).send(err.message)
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  try {
+    res.status(200).json({ success: true });
+  } catch (err: any) {
+    res.status(500).send(err.message);
   }
-})
+});
 
-app.listen(PORT, () => {console.log("Listening on port "+PORT)})
+app.post("/github-event", (req: Request, res: Response) => {
+  try {
+    const { action, sender, repository } = req.body;
+    const event = req.header("x-github-event");
+    switch (event) {
+      case "star":
+        console.log(
+          `${sender.login} ${action} star on ${repository.full_name}`
+        );
+        break;
+      case "issues":
+        const { issue } = req.body;
+        console.log(
+          `${sender.login} ${action} issue ${issue} on ${repository.full_name}`
+        );
+        break;
+      case "push":
+        console.log(`${sender.login} pushes on ${repository.full_name}`);
+        break;
+      default:
+        console.log(`evento desconocido: ${event}`);
+        break;
+    }
+    res.status(200).json({
+      success: true,
+    });
+  } catch (err: any) {
+    res.status(500).send(err.message);
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port " + 3000);
+});
