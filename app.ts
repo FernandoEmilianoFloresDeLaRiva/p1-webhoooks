16ad88eb58cfbe6fedb7ca679945e241b8b14fa2
FernandoEmilianoFloresDeLaRiva <emilianoflores07081@gmail.com>
2024-02-07 08:47:27 -0600

    aa

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import { URL_WEBHOOK_DISCORD } from "./src/constants/url_webhook_discord";
 
 const app = express();
 
@@ -14,29 +15,36 @@ app.get("/", (req: Request, res: Response) => {
   }
 });
 
+//recibiendo datos de un webhook
 app.post("/github-event", (req: Request, res: Response) => {
   try {
     const { action, sender, repository } = req.body;
     const event = req.header("x-github-event");
+    let message = "";
     switch (event) {
       case "star":
         console.log(
           `${sender.login} ${action} star on ${repository.full_name}`
         );
+        message = `${sender.login} ${action} star on ${repository.full_name}`;
         break;
       case "issues":
         const { issue } = req.body;
         console.log(
           `${sender.login} ${action} issue ${issue.title} on ${repository.full_name}`
         );
+        message = `${sender.login} ${action} issue ${issue.title} on ${repository.full_name}`;
         break;
       case "push":
         console.log(`${sender.login} pushes on ${repository.full_name}`);
+        message = `${sender.login} pushes on ${repository.full_name}`;
         break;
       default:
         console.log(`evento desconocido: ${event}`);
+        message = `evento desconocido: ${event}`;
         break;
     }
+    fetchNotifyDiscord(message);
     res.status(200).json({
       success: true,
     });
@@ -44,6 +52,24 @@ app.post("/github-event", (req: Request, res: Response) => {
     res.status(500).send(err.message);
   }
 });
+
+//mandando datos a un webhook
+const fetchNotifyDiscord = async (reqUser: string) => {
+  try {
+    const body = {
+      content: reqUser,
+    };
+    const bodyReq = {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(body),
+    };
+    const response = await fetch(URL_WEBHOOK_DISCORD, bodyReq);
+    if (!response?.ok) throw new Error("Error enviando datos a discord");
+  } catch (err: any) {
+    throw new Error(err.message);
+  }
+};
 
 app.listen(3000, () => {
   console.log("Listening on port " + 3000);
---
+++ src/constants/url_webhook_discord.ts
@@ -0,0 +1,2 @@
+export const URL_WEBHOOK_DISCORD: string =
+  "https://discord.com/api/webhooks/1204794472668598282/lBNKhgLQCFVtjwmS1jEpzjhg2SZgFA0wN36hHXG8v6t_hFmZCQTOQ7STtaYfPVMTS7b3";
