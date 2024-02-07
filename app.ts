8da8757cc6b7cc90aa8ae05b828d9ab8ce1504c8
FernandoEmilianoFloresDeLaRiva <emilianoflores07081@gmail.com>
2024-02-07 09:51:02 -0600

    auth con git

--- app.ts
+++ app.ts
@@ -1,6 +1,8 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import { URL_WEBHOOK_DISCORD } from "./src/constants/url_webhook_discord";
+import crypto from "crypto";
+import { SECRET_WEBHOOK } from "./src/constants/secret_webhook";
 
 const app = express();
 
@@ -16,42 +18,46 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 //recibiendo datos de un webhook
-app.post("/github-event", (req: Request, res: Response) => {
-  try {
-    const { action, sender, repository } = req.body;
-    const event = req.header("x-github-event");
-    let message = "";
-    switch (event) {
-      case "star":
-        console.log(
-          `${sender.login} ${action} star on ${repository.full_name}`
-        );
-        message = `${sender.login} ${action} star on ${repository.full_name}`;
-        break;
-      case "issues":
-        const { issue } = req.body;
-        console.log(
-          `${sender.login} ${action} issue ${issue.title} on ${repository.full_name}`
-        );
-        message = `${sender.login} ${action} issue ${issue.title} on ${repository.full_name}`;
-        break;
-      case "push":
-        console.log(`${sender.login} pushes on ${repository.full_name}`);
-        message = `${sender.login} pushes on ${repository.full_name}`;
-        break;
-      default:
-        console.log(`evento desconocido: ${event}`);
-        message = `evento desconocido: ${event}`;
-        break;
+app.post(
+  "/github-event",
+  verifySignatureMiddleware,
+  (req: Request, res: Response) => {
+    try {
+      const { action, sender, repository } = req.body;
+      const event = req.header("x-github-event");
+      let message = "";
+      switch (event) {
+        case "star":
+          console.log(
+            `${sender.login} ${action} star on ${repository.full_name}`
+          );
+          message = `${sender.login} ${action} star on ${repository.full_name}`;
+          break;
+        case "issues":
+          const { issue } = req.body;
+          console.log(
+            `${sender.login} ${action} issue ${issue.title} on ${repository.full_name}`
+          );
+          message = `${sender.login} ${action} issue ${issue.title} on ${repository.full_name}`;
+          break;
+        case "push":
+          console.log(`${sender.login} pushes on ${repository.full_name}`);
+          message = `${sender.login} pushes on ${repository.full_name}`;
+          break;
+        default:
+          console.log(`evento desconocido: ${event}`);
+          message = `evento desconocido: ${event}`;
+          break;
+      }
+      fetchNotifyDiscord(message);
+      res.status(200).json({
+        success: true,
+      });
+    } catch (err: any) {
+      res.status(500).send(err.message);
     }
-    fetchNotifyDiscord(message);
-    res.status(200).json({
-      success: true,
-    });
-  } catch (err: any) {
-    res.status(500).send(err.message);
   }
-});
+);
 
 //mandando datos a un webhook
 const fetchNotifyDiscord = async (reqUser: string) => {
@@ -70,6 +76,39 @@ const fetchNotifyDiscord = async (reqUser: string) => {
     throw new Error(err.message);
   }
 };
+
+const verify_signature = (req: Request) => {
+  try {
+    const signature = crypto
+      .createHmac("sha256", SECRET_WEBHOOK)
+      .update(JSON.stringify(req.body))
+      .digest("hex");
+    const trusted = Buffer.from(`sha256=${signature}`, "ascii");
+    const untrusted = Buffer.from(
+      req.header("x-hub-signature-256") ?? "",
+      "ascii"
+    );
+    return crypto.timingSafeEqual(trusted, untrusted);
+  } catch (err: any) {
+    throw new Error(err);
+  }
+};
+
+function verifySignatureMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    verify_signature(req);
+    next();
+  } catch (err) {
+    res.status(401).json({
+      success: false,
+      message: "unauthorized",
+    });
+  }
+}
 
 app.listen(3000, () => {
   console.log("Listening on port " + 3000);
